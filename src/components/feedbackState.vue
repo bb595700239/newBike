3b60cc8d5a14a80a6d0d667891fcb399680147ee
Joubn <595700239@qq.com>
2017-02-19 14:05:29 +0800

    平台添加

--- src/components/feedbackState.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <mt-button type="default" disabled size="small" v-show="item.feedbackState==2">已处理</mt-button>
-  <mt-button type="danger" size="small" v-show="item.feedbackState==0" @click.native="setprocessed(item.feedbackId,index)">设为已处理</mt-button>
-</template>
-
-
-
